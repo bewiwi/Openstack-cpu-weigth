0b0fff09d0888fad32b3515ba32ac40122a132dc
Loïc PORTE <loic.porte@bibabox.fr>
2014-07-23 11:41:21 +0200

    Typo

--- cpu.py
+++ cpu.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 """
-RAM Weigher.  Weigh hosts by their RAM usage.
+CPU Weigher.  Weigh hosts by their CPU usage.
 
 The default is to spread instances across all hosts evenly.  If you prefer
 stacking, you can set the 'ram_weight_multiplier' option to a negative
@@ -37,7 +37,7 @@ CONF.register_opts(cpu_weight_opts)
 LOG = logging.getLogger(__name__)
 
 class CPUWeigher(weights.BaseHostWeigher):
-    minval = -5000000
+    minval = 0
 
     def weight_multiplier(self):
         """Override the weight multiplier."""
@@ -45,8 +45,5 @@ class CPUWeigher(weights.BaseHostWeigher):
 
     def _weigh_object(self, host_state, weight_properties):
         """Higher weights win.  We want spreading to be the default."""
-
-        LOG.warn('EEEEEEE')
-        LOG.warn('TOTAL %s' % host_state.vcpus_total)
-        LOG.warn('USED %s' % host_state.vcpus_used)
+        LOG.debug('vCPU avaible %s' % (host_state.vcpus_total - host_state.vcpus_used))
         return host_state.vcpus_total - host_state.vcpus_used
